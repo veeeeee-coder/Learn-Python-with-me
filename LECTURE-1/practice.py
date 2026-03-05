3a06f40c4339e6d5cce0a1c87fef101911046167
veeeeee-coder <vijayeshwaridesu@gmail.com>
2026-03-05 22:33:08 +0530

    Lecture-1

---
+++ LECTURE-1/practice.py
@@ -0,0 +1,23 @@
+a=int(input("enter a number: "))
+b=int(input("enter another number: "))
+c=a+b
+print("the sum of", a, "and", b, "is", c)
+
+
+s1=int(input("enter side 1: "))
+s2=int(input("enter side 2: "))
+print("area of square is", s1*s2)
+
+f1=float(input("enter first number: "))
+f2=float(input("enter second number: "))
+c=f1+f2/2
+print("the average of", f1, "and", f2, "is", c)
+
+
+a1=int(input("enter a number: "))
+b1=int(input("enter another number: "))
+if(a1>=b1):
+    print(True)
+else:
+    print(False)
+#print(a>=b)
