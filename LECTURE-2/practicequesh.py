01b30a407033354aef8a3fd030e08a2556990fda
veeeeee-coder <vijayeshwaridesu@gmail.com>
2026-03-06 13:44:22 +0530

    Lecture-2

---
+++ LECTURE-2/practicequesh.py
@@ -0,0 +1,27 @@
+#odd or even
+n=int(input("enter a number"))
+if(n%2==0):
+    print("eveennnnnn")
+else:
+    print("oddddddd")
+
+
+#greatest of three nos
+a=int(input("enter a"))
+b=int(input("enter b"))
+c=int(input("enter c"))
+if(a>=b and a>=c):
+    print("a is greater")
+elif(b>=a and b>=c):
+    print("b is greater")
+else:
+    print("c is greater")
+
+
+num1=int(input("enter a numm"))
+if(num1%7==0):
+    print("its div by 7")
+else:
+    print("not divvie")
+
+    
---
+++ LECTURE-2/practicequestions.py
@@ -0,0 +1,9 @@
+#wap to input users first name and print its length
+s=input("enter ur name")
+print(len(s))
+
+#wap to find occurence of '$' in string
+s="hi $ i am $ and i love $"
+print(s.find("$"))
+print(s.count("$"))
+
